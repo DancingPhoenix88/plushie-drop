937254de0397d62e899a688b0abf24a9fb644f77
HoangVP <hvuphuongext-gif>
2026-05-30 18:18:41 +0700

    Initial commit - Plushie Drop

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE = 'plushie-v1';
+const FILES = [
+  './index.html',
+  './manifest.json',
+  './Sprites/level_1.png',
+  './Sprites/level_2.png',
+  './Sprites/level_3.png',
+  './Sprites/level_4.png',
+  './Sprites/level_5.png',
+  './Sprites/level_6.png',
+  './Sprites/level_7.png',
+  './Sprites/level_8.png',
+  './Sprites/level_9.png',
+  './Sprites/level_10.png',
+  './Sprites/level_11.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      });
+    })
+  );
+});
